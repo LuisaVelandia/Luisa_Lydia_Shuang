9c77a239c0e35145e1f9774b7f7f455cecf1c5e3
LuisaVelandia <luisa.velandia.it@gmail.com>
2022-10-20 17:21:27 -0500

    Initial commit

---
+++ src/main/java/com/example/luisa_lydia_shuang/LuisaLydiaShuangApplication.java
@@ -0,0 +1,13 @@
+package com.example.luisa_lydia_shuang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuisaLydiaShuangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuisaLydiaShuangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/luisa_lydia_shuang/LuisaLydiaShuangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.luisa_lydia_shuang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuisaLydiaShuangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
